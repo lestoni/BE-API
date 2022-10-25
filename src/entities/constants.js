a5888f07bbabcc12bf470c67e385dd847dfc0b8a
Tonny Mutai <tonnimutai@gmail.com>
2022-10-25 19:26:08 +0300

    feat: profile type constants

--- src/entities/constants.js
+++ src/entities/constants.js
@@ -2,8 +2,15 @@ const CONTRACT_STATUS = {
   TERMINATED: 'terminated',
   NEW: 'new',
   IN_PROGRESS: 'in_progress'
+};
+
+const PROFILE_TYPE = {
+  CLIENT: 'client',
+  CONTRACTOR: 'contractor',
+  ADMIN: 'admin'
 }
 
 module.exports = {
-  CONTRACT_STATUS
+  CONTRACT_STATUS,
+  PROFILE_TYPE
 }
