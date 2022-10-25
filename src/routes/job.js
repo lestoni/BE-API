d95fb1b65c436c403402d39a4a3bd8e96ef255f8
Tonny Mutai <tonnimutai@gmail.com>
2022-10-25 19:27:38 +0300

    feat: add job router

--- src/routes/job.js
+++ src/routes/job.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fetchActiveUnpaidJobs } = require('../controllers');
+const { fetchActiveUnpaidJobs, payContractor } = require('../controllers');
 
 const router = express.Router();
 
@@ -15,8 +15,42 @@ const router = express.Router();
   const {id: profileId } = req.profile;
 
   try {
-    const jobs = await fetchActiveUnpaidJobs(Contract, Job, profileId);
+    const jobs = await fetchActiveUnpaidJobs({
+      Job,
+      Contract
+    }, profileId);
+
     res.json(jobs);
+  } catch(error) {
+    res.status(500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  }
+});
+
+/**
+ * Client Paying a contractor for a job well done
+ * 
+ * GET /jobs/:job_id/pay
+ * 
+ * @returns contracts for user in session
+ */
+ router.post('/:job_id/pay', async (req, res) =>{
+  const {Job, Contract, Profile } = req.app.get('models');
+  const sequelize = req.app.get('sequelize');
+  // FIXME: Use data validation for inputs
+  const { amountToPay } = req.body; // Worth to note we are working with money value hence .2f
+  const {job_id} = req.params;
+
+  try {
+    const paymentStatus = await payContractor({
+      Contract, Job, Profile, sequelize
+    }, job_id, amountToPay);
+
+    res.json({ success: paymentStatus });
   } catch(error) {
     res.status(500);
     res.json({
